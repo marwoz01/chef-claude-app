9482ccd6f252e96f679a790af6f8e0f3494724a2
Marcel Wozniak <marwozniak01@gmail.com>
2026-01-02 08:18:55 +0100

    quick fixes to deploy on vercel

--- src/ai.js
+++ src/ai.js
@@ -1,5 +1,5 @@
 export async function getRecipeFromChefClaude(ingredientsArr) {
-  const res = await fetch("http://localhost:3001/api/recipe", {
+  const res = await fetch("/api/recipe", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ ingredients: ingredientsArr }),
